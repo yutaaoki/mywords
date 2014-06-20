4ac8a12f3908158a21816f67d8c3071fe93d13a5
yuta <y_aoki@ubicast.com>
2014-06-20 09:50:02 +0000

    watch scope changes from a directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -25,4 +25,51 @@ angular.module('myWordsApp.controllers', [])
     });
   };
 
+  $scope.share = function () {
+    ezfb.ui(
+      {
+        method: 'feed',
+        name: 'angular-easyfb API demo',
+        picture: 'http://plnkr.co/img/plunker.png',
+        link: 'http://plnkr.co/edit/qclqht?p=preview',
+        description: 'angular-easyfb is an AngularJS module wrapping Facebook SDK.' + 
+                     ' Facebook integration in AngularJS made easy!' + 
+                     ' Please try it and feel free to give feedbacks.'
+      },
+      function (res) {
+        // res: FB.ui response
+      }
+    );
+  };
+
+  /**
+   * For generating better looking JSON results
+   */
+  var autoToJSON = ['loginStatus', 'apiMe']; 
+  angular.forEach(autoToJSON, function (varName) {
+    $scope.$watch(varName, function (val) {
+      $scope[varName + 'JSON'] = JSON.stringify(val, null, 2);
+    }, true);
+  });
+  
+  /**
+   * Update loginStatus result
+   */
+  function updateLoginStatus (more) {
+    ezfb.getLoginStatus(function (res) {
+      $scope.loginStatus = res;
+
+      (more || angular.noop)();
+    });
+  }
+
+  /**
+   * Update api('/me') result
+   */
+  function updateApiMe () {
+    ezfb.api('/me', function (res) {
+      $scope.apiMe = res;
+    });
+  }
+
 }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -4,18 +4,21 @@
 
 
 angular.module('myWordsApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]).
-  directive('mainCanvas', function($log) {
-    function link(scope, elm, attrs) {
+directive('appVersion', ['version', function(version) {
+  return function(scope, elm, attrs) {
+    elm.text(version);
+  };
+}]).
+directive('mainCanvas', function($log) {
+
+
+  function link(scope, elm, attrs) {
+    scope.$watch('apiMe', function(value){
       WordCloud(elm[0], { list: [['love', 20], ['hate',10]]});
+    });
+  };
 
-    };
-
-    return {
-        link: link
-    };
-  });
+  return {
+    link: link
+  };
+});
