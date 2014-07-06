326925c5450579bd09005e5a5ed5faf7119b9fd7
Yuta <y_aoki@ubicast.com>
2014-07-06 05:07:28 +0000

    clean up

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,24 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
+// Declare app
 angular.module('myWordsApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
   'myWordsApp.directives',
   'myWordsApp.controllers',
   'myWordsApp.config',
   'ezfb'
-]).
-config(['$routeProvider', function($routeProvider) {
+])
+
+// App config
+.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
   $routeProvider.when('/cloud/:user', {templateUrl: 'partials/main.html', controller: 'MainCtrl'});
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
   $routeProvider.otherwise({redirectTo: '/'});
-}]).config(['ezfbProvider', 'CONF', function (ezfbProvider, CONF) {
+}])
+
+// ezfb config
+.config(['ezfbProvider', 'CONF', function (ezfbProvider, CONF) {
   ezfbProvider.setInitParams({
     appId: CONF.appId
   });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,13 +1,12 @@
 'use strict';
 
-/* Controllers */
-
+// Controllers
 angular.module('myWordsApp.controllers', [])
 
+//** Login page **//
 .controller('LoginCtrl', ['$scope','$log', 'ezfb', '$location', function($scope, $log, ezfb, $location) {
-  /**
-   * Update loginStatus result
-   */
+
+  // Redirect if connected
   var updateLoginStatus = function () {
     ezfb.getLoginStatus(function (res) {
       if(res.status == 'connected'){
@@ -17,8 +16,7 @@ angular.module('myWordsApp.controllers', [])
     });
   }
 
-  updateLoginStatus();
-
+  // Login button
   $scope.login = function () {
     // Calling FB.login with required permissions specified
     ezfb.login(function (res) {
@@ -27,9 +25,13 @@ angular.module('myWordsApp.controllers', [])
       }
     }, {scope: 'email,user_likes,read_mailbox'});
   };
-  
+
+  // Update status on loading
+  updateLoginStatus();
 }])
 
+
+//** Main page (word cloud) **//
 .controller('MainCtrl', ['$scope','$log', 'ezfb', '$routeParams', '$location', '$http', 'CONF', function($scope, $log, ezfb, $routeParams, $location, $http, CONF) {
 
   //Set 'me' if empty
@@ -54,27 +56,38 @@ angular.module('myWordsApp.controllers', [])
 
   // Callback: make wordfreq list and set it
   var setFreqListCB = function(data){
+
+    // Additional options
     var options = {
       workerUrl: 'lib/wordfreq/src/wordfreq.worker.js',
       filterSubstring: true,
       minimumCount: 4,
       stopWords: ['im', 'do']
     };
+
+    // Analyse the text and make a frequency list
     WordFreq(options)
       .process(data.data, function (list){
         $scope.freqList = list;
+        // Notify the word cloud watcher
         $scope.$apply();
       });
   };
 
-  // Get text and make a freq list
+  // Render the world cloud on load
   ezfb.getLoginStatus(function (res) {
     if(!checkLoginCB(res)){
       return;
     }
+
+    // Access token to pass to the server
     var accessToken = res.authResponse.accessToken;
+
+    // Get the user id
     ezfb.api('/me', function(me) {
       setMeIdCB(me);
+
+      // Get the message text
       $http.get(CONF.apiUrl+'/messages/'+meId+'?access_token='+accessToken).success(function(data){
         setFreqListCB(data);
       });
